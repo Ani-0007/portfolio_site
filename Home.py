6142b18a73a4f8ea9fa147f3f8b69366b6d1043a
Anirudh <anirudhkocherlakota@gmail.com>
2023-09-13 23:42:39 +0530

    Beautifying

--- Home.py
+++ Home.py
@@ -2,30 +2,34 @@ import streamlit as st
 import pandas
 
 st.set_page_config(layout="wide")
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1,2])
 
 with col1:
-    st.image("images/AniPhoto_Glasses.jpg")
-
-    content2 = "Below apps are built in Python. Feel free to contact me!"
-    st.write(content2)
+    st.image("images/AniPhoto_Glasses_Croped.jpg", width=300)
 
 with col2:
     st.title("Anirudh Kocherlakota")
-    content = """Hi, I am Anirudh! I am a Python programmer   """
+    content = """Hi, I am Anirudh! I am a Python programmer.
+     I am passionate about Machine Learning and Artificial Intelligence. 
+     I am currently trying to get a deep understanding of basics of AI and ML.
+     """
     st.info(content)
 
+st.markdown("---")
+content2 = '''Below apps are built in Python. Feel free to contact me!'''
+st.info(content2)
 
-col3, emptycol, col4 = st.columns([1.5, 0.5, 1.5])
+
+col3, emptycol1, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
-
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
+        st.text("\n")
 
 with col4:
     for index, row in df[10:].iterrows():
@@ -33,3 +37,4 @@ with col4:
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source Code]({row['url']})")
+        st.text("\n")
